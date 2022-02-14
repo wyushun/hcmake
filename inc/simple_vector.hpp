a12203d6a638a62bbf54fe30e188bab35d5e53a1
wyushun <wyushun@foxmail.com>
2022-02-14 16:27:08 +0800

    add operator++(int)

--- inc/simple_vector.hpp
+++ inc/simple_vector.hpp
@@ -23,12 +23,29 @@ class SimpleVector {
 
   bool full() const { return last_.ptr_ == end_.ptr_; }
   bool empty() const { return first_.ptr_ == last_.ptr_; }
+  uint32_t size() const { return last_.ptr_ - first_.ptr_; }
+  T& operator[](int idx) {
+    CHECK(idx < size());
+    return first_.ptr_[idx];
+  }
+  const T& operator[](int idx) const {
+    CHECK(idx < size());
+    return first_.ptr_[idx];
+  }
 
   class iterator {
    public:
     friend class SimpleVector;
     iterator(T* ptr = nullptr) : ptr_(ptr) {}
-    void operator++() { ++ptr_; }
+    iterator& operator++() {
+      ptr_++;
+      return *this;
+    }
+    iterator operator++(int) {
+      iterator tmp(ptr_);
+      ptr_++;
+      return tmp;
+    }
     bool operator!=(const iterator& it) { return ptr_ != it.ptr_; }
     T& operator*() { return *ptr_; }
     T* operator->() { return ptr_; }
@@ -40,11 +57,6 @@ class SimpleVector {
   iterator end() { return iterator(last_.ptr_); }
 
  private:
-  iterator first_;
-  iterator last_;
-  iterator end_;
-  Alloc allocator_;
-
   void resize() {
     if (first_.ptr_ == nullptr) {
       first_.ptr_ = allocator_.allocate(1);
@@ -63,4 +75,10 @@ class SimpleVector {
       end_.ptr_ = first_.ptr_ + 2 * size;
     }
   }
+
+ private:
+  iterator first_;
+  iterator last_;
+  iterator end_;
+  Alloc allocator_;
 };
--- test/test_simple_vector.cpp
+++ test/test_simple_vector.cpp
@@ -1,5 +1,15 @@
 #include "simple_vector.hpp"
 
 int main(int argc, char**argv) {
+  SimpleVector<int> sv;
+  sv.push_back(100);
+  sv.push_back(1000);
+  sv.push_back(1000);
+  sv.push_back(10000);
+
+  for (auto it = sv.begin(); it != sv.end(); it++) {
+    cout << "total size: " << sv.size() << ", cur value: " << *it << endl;
+  }
+
   return 0;
 }
