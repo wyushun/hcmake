4e6ebd70973c694909e8f60e445bc69fafaeed0e
wyushun <wyushun@foxmail.com>
2022-03-06 14:42:37 +0800

    define -DPYTHON_EXECUTABLE in compile.sh to fix py11 cl err

--- compile.sh
+++ compile.sh
@@ -12,6 +12,7 @@ cmake \
   -DCMAKE_BUILD_TYPE=Debug \
   -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+  -DPYTHON_EXECUTABLE=`which python3` \
   ..
 cmake --build ${BUILD_DIR} -j16
 
