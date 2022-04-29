784aad2de6acb3d5367e90f0bb345b07137dd014
wyushun <wyushun@foxmail.com>
2022-04-29 14:35:15 +0800

    update

--- test/general_example.cpp
+++ test/general_example.cpp
@@ -2,21 +2,21 @@
 #include "std_inc.h"
 
 int main(int argc, char** argv) {
-  cout << "Executing npu_aol_attach......" << endl;
+  cout << "Executing npu_aol_attach......\n";
   auto* handle = npu_aol_attach(0, NPU_SCHEDULE_MODE_SINGLE);
   if (!handle) {
-    cerr << "npu_aol_attach error!" << endl;
+    cerr << "npu_aol_attach error!\n";
     return 1;
   }
-  cout << "npu_aol_attach success: \n"
-       << "\taol version: 0x" << std::hex << handle->aol_version
-       << "\tcore count: 0x" << std::hex << handle->core_count
-       << "\tcore phy addr: 0x" << std::hex << handle->core_phy_addr;
+  cout << "npu_aol_attach success: "
+       << "\n\taol version: 0x" << std::hex << handle->aol_version
+       << "\n\tcore count: 0x" << std::hex << handle->core_count
+       << "\n\tcore phy addr: 0x" << std::hex << handle->core_phy_addr << "\n";
 
-  cout << "Executing npu_aol_detach......" << endl;
+  cout << "Executing npu_aol_detach......\n";
   auto ret = npu_aol_detach(handle);
   if (ret != NPU_AOL_OK) {
-    cerr << "npu_aol_detach error!" << endl;
+    cerr << "npu_aol_detach error!\n";
     return 1;
   }
   cout << "npu_aol_detach success!\n";
