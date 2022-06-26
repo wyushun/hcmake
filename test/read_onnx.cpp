7264777f6e1a616e68219808826b5da85ea608e0
wyushun <wyushun@foxmail.com>
2022-06-26 17:31:00 +0800

    add basic file

--- test/read_onnx.cpp
+++ test/read_onnx.cpp
@@ -1,8 +1,17 @@
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/message.h>
+#include <google/protobuf/text_format.h>
+
 #include "basis.hpp"
 #include "cxxopts.hpp"
+#include "onnx.pb.h"
 
 // clang-format off
-void parse(int argc, char** argv, uint64_t &size) {
+namespace {
+string model;
+
+void parse(int argc, char** argv) {
   try {
     cxxopts::Options options(argv[0], " - npu command line options");
     options.positional_help("[optional args]").show_positional_help();
@@ -10,7 +19,7 @@ void parse(int argc, char** argv, uint64_t &size) {
     options.set_width(70)
       .set_tab_expansion()
       .add_options()
-      ("s,size", "Memory size(MB) to be operated", cxxopts::value<uint64_t>())
+      ("m,model", "onnx model", cxxopts::value<string>())
       ("h,help", "Print all command usages");
 
     auto result = options.parse(argc, argv);
@@ -19,8 +28,8 @@ void parse(int argc, char** argv, uint64_t &size) {
       exit(0);
     }
 
-    if (result.count("size")) {
-      size = result["size"].as<uint64_t>();
+    if (result.count("model")) {
+      model = result["model"].as<string>();
     } else {
       std::cout << options.help() << std::endl;
       exit(0);
@@ -31,26 +40,11 @@ void parse(int argc, char** argv, uint64_t &size) {
     exit(1);
   }
 }
+}
 // clang-format on
 
 int main(int argc, char** argv) {
-  cout << "test cxxopts..." << endl;
-  uint64_t size;
-  parse(argc, argv, size);
+  parse(argc, argv);
 
-  cout << "test any..." << endl;
-  any t = size;
-  cout << "any value is: " << any::get<uint64_t>(t) << endl;
-
-  cout << "test optional..." << endl;
-  optional<string> s = "hello world!";
-  if (s.has_value()) {
-    cout << "optional value is: " << s.value() << endl;
-  }
-
-  optional<any> t2 = t;
-  if (t2.has_value()) {
-    cout << "optional value is: " << any::get<uint64_t>(t2.value()) << endl;
-  }
   return 0;
 }
