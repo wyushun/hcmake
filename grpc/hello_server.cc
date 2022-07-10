700cc0f500f1d878bea89fe4a2dfa4b992adc206
wyushun <wyushun@foxmail.com>
2022-07-10 09:04:11 +0800

    optimize grpc folder and cmake

--- grpc/hello_server.cc
+++ grpc/hello_server.cc
@@ -1,10 +1,10 @@
+#include <grpcpp/grpcpp.h>
+
 #include <iostream>
 #include <memory>
 #include <string>
 
-#include <grpcpp/grpcpp.h>
-
-#include "helloworld.grpc.pb.h"
+#include "hello.grpc.pb.h"
 
 using grpc::Server;
 using grpc::ServerBuilder;
